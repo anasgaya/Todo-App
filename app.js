f2980cf553dac1beccf985b83c6f648000960d5a
anasgaya <anaasraheel.123@gmail.com>
2024-08-10 02:16:30 -0700

    completed

--- app.js
+++ app.js
@@ -34,30 +34,30 @@ function deleteTodo(index){
     `
 }
 }
-    todo.splice(index , 1)
-    list.innerHTML = '';
-    for (var index = 0; index < todo.length; index++) {
-    list.innerHTML += `
-    <li>
-    ${todo[index]}
-    <button onclick = "deleteTodo(${index})">Delete</button>
-    <button onclick = "editTodo()">Edit</button>
-    </li>
-    `
-}
+//     todo.splice(index , 1)
+//     list.innerHTML = '';
+//     for (var index = 0; index < todo.length; index++) {
+//     list.innerHTML += `
+//     <li>
+//     ${todo[index]}
+//     <button onclick = "deleteTodo(${index})">Delete</button>
+//     <button onclick = "editTodo()">Edit</button>
+//     </li>
+//     `
+// }
 
 
 
-    function editTodo(){
-    var addValue = prompt('enter updated value');
-    todo.splice(index,1,addValue)
-    for (var index = 0; index < todo.length; index++) {
-        list.innerHTML += `
-        <li>
-        ${todo[index]}
-        <button onclick = "deleteTodo(${index})">Delete</button>
-        <button onclick = "editTodo()">Edit</button>
-        </li>
-        `
-    }
-}
+//     function editTodo(){
+//     var addValue = prompt('enter updated value');
+//     todo.splice(index,1,addValue)
+//     for (var index = 0; index < todo.length; index++) {
+//         list.innerHTML += `
+//         <li>
+//         ${todo[index]}
+//         <button onclick = "deleteTodo(${index})">Delete</button>
+//         <button onclick = "editTodo()">Edit</button>
+//         </li>
+//         `
+//     }
+// }
